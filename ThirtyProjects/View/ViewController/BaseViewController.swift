a1da3b427e16a5f7111937d01621979065f4c6f8
HEECHAN CHO <wkfyd6263@gmail.com>
2024-06-22 18:47:11 +0900

    [Main] Initialize project with MVVM structure

---
+++ ThirtyProjects/View/ViewController/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  ThirtyProjects
+//
+//  Created by CHOHEECHAN on 6/22/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
